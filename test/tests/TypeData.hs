224f63dcbc09f2b2f94c241e95a0c3346382271b
sheaf <sam.derbyshire@gmail.com>
2026-06-29 03:46:36 +0200

    Fixes for TypeData over multiple lines

--- test/tests/TypeData.hs
+++ test/tests/TypeData.hs
@@ -3,14 +3,17 @@
 {-# LANGUAGE TypeData #-}
 
    type data D1 = MkD1
+-- ^^^^ keyword.other.type.haskell
 -- ^^^^^^^^^^^^^^^^^^^ meta.declaration.data.algebraic.haskell
 --      ^^^^ keyword.other.data.haskell
 
    type data D2 = MkD2_A | MkD2_B
+-- ^^^^ keyword.other.type.haskell
 -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.declaration.data.algebraic.haskell
 --      ^^^^ keyword.other.data.haskell
 
    type data D3 where { {- hiho -} MkD3 :: D3 }
+-- ^^^^ keyword.other.type.haskell
 --      ^^^^ keyword.other.data.haskell
 --              ^^^^^ keyword.other.where.haskell
 --              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.declaration.data.generalized.haskell
@@ -19,7 +22,33 @@
 -- thing as a type data declaration (due to TextMate limitations).
 -- The following test ensures we at least highlight it reasonably.
    type
+-- ^^^^ keyword.other.type.haskell
      data D4 = MkD4
 --   ^^^^^^^^^^^^^^ meta.declaration.data.algebraic.haskell
 --   ^^^^ keyword.other.data.haskell
 --   ^^^^ - variable.other.generic-type.haskell
+
+   type
+-- ^^^^ keyword.other.type.haskell
+                 data
+--               ^^^^ keyword.other.data.haskell
+             D5
+--           ^^ storage.type.haskell
+      where
+--    ^^^^^ keyword.other.where.haskell
+         MkD5 :: D5
+--       ^^^^ constant.other.haskell
+--               ^^ storage.type.haskell
+
+   type
+-- ^^^^ keyword.other.type.haskell
+                 data
+--               ^^^^ keyword.other.data.haskell
+             D6
+--           ^^ storage.type.haskell
+      where {
+--    ^^^^^ keyword.other.where.haskell
+    MkD6 :: D6
+--  ^^^^ constant.other.haskell
+--          ^^ storage.type.haskell
+      }
